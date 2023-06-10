34adbe6a6f1216c859bc4e01b1adab05f9336bc2
Samuel Thompson <thomps9012@gmail.com>
2023-06-10 07:42:14 -0400

    initial commit with directions

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# CODE / API
+    # Use CMA Open Access API to extra a "highlight" based on user criteria from a CMA exhibition of the user's choosing
+        # highlight examples -> [biggest, smallest, oldest, longest, ...]
+        # exhibition must have at least 8 CMA-owned works
+    # Use the API to find 5 artworks that are related by one or more criteria to create a "mini exhibition"
+    # Set up a MongoDB Atlas API to store and persist the following data:
+        # athena id
+        # accession number
+        # tombstone
+        # image
+        # any fields used for highlight / criteria
+        # an identifier for the mini exhibition
+
+# README FILE
+    # How did you approach this problem and why you end up with the solution you did?
+    # How long did it take you to complete?
+    # How would you change your solution in order to scale it up to a web application where users can select an exhibition, highlight and similarity criteria, and receive results?
+    # How would you set this up as a process that runs daily, with different results, and posts them to social media platforms via API?
